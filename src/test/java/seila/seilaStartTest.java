5f278c8b953d5843f564aa09062024c6799ddc72
luclujan7198 <Kilzone 10>
2019-03-31 22:00:20 -0300

    versao inicial

---
+++ src/test/java/seila/seilaStartTest.java
@@ -0,0 +1,16 @@
+package seila;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class seilaStartTest {
+
+	@Test
+	public void test() {
+		assertEquals(1, 1);
+		//fail("Not yet implemented");`
+		//teste
+	}
+
+}
